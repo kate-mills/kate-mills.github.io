b3d4dfa83428bf8a9a264cf02b0daaae418b96c4
Kate Mills <kate.mills1124@gmail.com>
2017-12-26 19:41:37 -0800

    friendly footer

--- bookapp/js/footer.js
+++ bookapp/js/footer.js
@@ -1,50 +1,39 @@
-var day;
-var time;
-getDate();
-var h = h;
-var m = m;
-var time = time;
 
+function friendlyFooter(){
+    this.text = ' Happy '
 
-switch (new Date().getDay()) {
+    switch (new Date().getDay()) {
+
     case 0:
-        day = "Sunday";
-        text = ' It\'s not Sunday unless you are reading. ';
+        this.text += "Sunday."
         break;
     case 1:
-        day = "Monday";
-        text = ' It\'s Monday. Let\'s Get this Party Started! ';
+        this.text += "Monday."
         break;
     case 2:
-        day = "Tuesday";
-        text = ' Happy Tuesday Book Lovers. ';
+        this.text += "Tuesday."
         break;
     case 3:
-        day = "Wednesday";
-        text = ' Happy Hump Day. ';
+        this.text += "hump day";
         break;
     case 4:
-        day = "Thursday";
-        text = ' Tomorrow is Friday. ';
+        this.text += "Thursday";
         break;
     case 5:
-        day = "Friday";
-        text = ' It\'s Finally Friday!';
+        this.text += "Friday";
         break;
     case  6:
-        day = "Saturday";
-        text = ' Sweet Saturday. ';
-
-
+        this.text += "Saturday";
+        break;
+    }
 }
+var friendlyFooter = new friendlyFooter();
 
-    var napaLibrary = '<a class="inside_footer" title="Napa Library" id="napaLibrary" target="_blank" href="https://napa.polarislibrary.com/polaris/logon.aspx?ctx=3.1033.0.0.5&Header=1"><span class="glyphicon glyphicon-link"> </span> napa library</a>';
+var napaLibrary = '<a class="inside_footer" title="Napa Library" id="napaLibrary" target="_blank" href="https://napa.polarislibrary.com/polaris/logon.aspx?ctx=3.1033.0.0.5&Header=1"><span class="glyphicon glyphicon-link"> </span> napa library</a>';
 
-    var countyOfNapa = '<a title="NAPA COUNTY CA" id="countyOfNapa" target="_blank" href="http://countyofnapa.org/library/"><span class="glyphicon glyphicon-link"> </span> county of napa</a>';
+var countyOfNapa = '<a title="NAPA COUNTY CA" id="countyOfNapa" target="_blank" href="http://countyofnapa.org/library/"><span class="glyphicon glyphicon-link"> </span> county of napa</a>';
 
-    var worldCat = '<a title="World Cat" id="worldCat" target="_blank" href="http://www.worldcat.org/"><span class="glyphicon glyphicon-link"> </span> worldcat</a>';
+var worldCat = '<a title="World Cat" id="worldCat" target="_blank" href="http://www.worldcat.org/"><span class="glyphicon glyphicon-link"> </span> worldcat</a>';
 
-    var footerText = '&copy; km-2016  ' + text + napaLibrary +  countyOfNapa + worldCat;
+var footerText = '&copy; km-2016  ' + friendlyFooter.text + napaLibrary + countyOfNapa + worldCat;
     $('#footer').html(footerText );
-
-
