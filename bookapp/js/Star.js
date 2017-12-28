f955d00a4370312a56ad4b67d6274fab87ccd81e
Kate Mills <kate.mills1124@gmail.com>
2017-12-27 17:34:47 -0800

    removed unused - click method

--- bookapp/js/Star.js
+++ bookapp/js/Star.js
@@ -8,7 +8,6 @@ var title = {
 };
 
 var Bookmark = Backbone.View.extend({
-
     blue: '<td title="Click to move to Books Read" class="glyphicon glyphicon-bookmark blue">Available</td>',
     green: '<td title="Click to move to Books On Order." class="glyphicon glyphicon-bookmark green">Want</td>',
     orange: '<td title="Click to move to Books Available." class="glyphicon glyphicon-bookmark orange">Order</td>',
@@ -23,11 +22,4 @@ var Rating = Backbone.View.extend({
     questionmark: '<td style="" title="What do you think?" class="wdyt">?</td>',
     full: '<td title="" class="glyphicon glyphicon-star grey"></td>',
     favorite: '<td title="Click if you didn\'t like this book." class="glyphicon glyphicon-thumbs-up purple"></td>',
-
-    click: function(){
-        this.trigger('onClickStar',{
-            'data-star': this.model.get('rating'),
-        });
-  },
-
 });
