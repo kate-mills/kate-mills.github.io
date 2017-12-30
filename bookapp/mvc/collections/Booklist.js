bc10a8b2385df5d9c69d2e5f10e2db4c93f16fd1
Kate Mills <kate.mills1124@gmail.com>
2017-12-30 08:44:49 -0800

    update all app.BookList methods

--- bookapp/mvc/collections/Booklist.js
+++ bookapp/mvc/collections/Booklist.js
@@ -2,19 +2,17 @@ app.BookList = Backbone.Collection.extend({
       model: app.Book,
       localStorage: new Store("backbone-book"),
       getFavoriteBooks: function(){return this.where({'rating': 'thumbsup'})},
-      getAllBooks: function(){ return app.bookList.filter(function(book){ return book;});},
-      getBooksIWant: function(){ return app.bookList.filter(function(book){return book.get('iWant'); });},
-      getBooksOnOrder: function(){ return app.bookList.filter(function(book){return book.get('onOrder'); });},
-      getBooksAvailable: function(){ return app.bookList.filter(function(book){return book.get('available'); });},
-      getBooksRead: function(){ return app.bookList.filter(function(book){return book.get('alreadyRead'); });}
+      getAllBooks: function(){return this.models},
+      getBooksIWant: function(){return this.filter(function(book){return book.get('iWant');});},
+      getBooksOnOrder: function(){return this.filter(function(book){return book.get('onOrder');});},
+      getBooksAvailable: function(){return this.filter(function(book){return book.get('available');});},
+      getBooksRead: function(){return this.filter(function(book){return book.get('alreadyRead');});}
 
-});//close app.BookList
-
+});
 
 app.bookList = new app.BookList({model: app.Book});
 app.bookList.fetch();
 
 if(!app.bookList.length){
     var firstbook = app.bookList.create();
-     console.log('firstbook created', firstbook);
 }
