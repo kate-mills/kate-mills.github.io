bcb061ddc65d29ff84c9adcf2017601d302241ba
Kate Mills <kate.mills1124@gmail.com>
2017-12-29 21:16:31 -0800

    removed list variables, firstbook props, update to app.getFavoriteBooks

--- bookapp/js/collections/appBookList.js
+++ bookapp/js/collections/appBookList.js
@@ -1,7 +1,7 @@
 app.BookList = Backbone.Collection.extend({
       model: app.Book,
       localStorage: new Store("backbone-book"),
-      getFavoriteBooks: function(){return this.filter(function(book) {return book.get('star');});},
+      getFavoriteBooks: function(){return this.where({'rating': 'thumbsup'})},
       getAllBooks: function(){ return app.bookList.filter(function(book){ return book.get('all');});},
       getBooksIWant: function(){ return app.bookList.filter(function(book){return book.get('iWant'); });},
       getBooksOnOrder: function(){ return app.bookList.filter(function(book){return book.get('onOrder'); });},
@@ -11,27 +11,10 @@ app.BookList = Backbone.Collection.extend({
 });//close app.BookList
 
 
-app.bookList = new app.BookList();
+app.bookList = new app.BookList({model: app.Book});
 app.bookList.fetch();
 
 if(!app.bookList.length){
-    var firstbook = app.bookList.create({
-        title: 'Add Some Books To Your Collection!',
-        author: 'Vince Flynn',
-        reader: 'Peter Hermann',
-        published: 2016,
-        iWant: true,
-        onOrder: false,
-        available: false,
-        alreadyRead: false
-      });
-      firstbook.set('created_at', Date());
-      console.log('book created', firstbook);
+    var firstbook = app.bookList.create();
+     console.log('firstbook created', firstbook);
 }
-
-
-var allBooks =app.bookList;
-var booksIWant =app.bookList.filter(function(book){ return book.get('iWant');});
-var booksOnOrder =app.bookList.filter(function(book){ return book.get('onOrder');});
-var booksAvailable =app.bookList.filter(function(book){ return book.get('available');});
-var booksRead =app.bookList.filter(function(book){ return book.get('read');});
