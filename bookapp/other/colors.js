1779d96ce0059b02e131450c97f025d47a07a2a9
Kate Mills <kate.mills1124@gmail.com>
2017-12-31 13:14:42 -0800

    update to js colors

--- bookapp/other/colors.js
+++ bookapp/other/colors.js
@@ -22,50 +22,46 @@ $.fn.tealify = function(){
     this.css('color', '#009688');
     return this;
 };
-$.fn.darkGrey = function(){
-    this.css("color", "#080808");
-    return this;
-};
 $.fn.lightGrey = function(){
     this.css('color', '#9e9e9e');
     return this;
 };
 function makeRed(){
     $('span.badge').removeClass('backGrey backGreen backFavorite backOrange backBlue').addClass('backRed');
-    $("th").eq(4).redify().text('Read');
-    $("th").eq(5).darkGrey().text('wdyt?');
+    $("th").eq(4).redify();
+    $("th").eq(5).redify();
     $("#ToggleArrow").redify();
     $('glyphicon glyphicon-bookmark').addClass('red');
 }
 function makeOrange(){
     $('span.badge').removeClass('backGrey backGreen backBlue backFavorite backRed').addClass('backOrange');
-    $("th").eq(4).orangeify().text('Order');
+    $("th").eq(4).orangeify();
     $("th").eq(5).neutralize();
     $("#ToggleArrow").orangeify();
 }
 function makeGreen() {
     $('span.badge').removeClass('backGrey backFavorite backBlue backOrange backRed').addClass('backGreen');
-    $("th").eq(4).greenify().text('Want');
+    $("th").eq(4).greenify();
     $("th").eq(5).neutralize();
     $("#ToggleArrow").greenify();
 }
 function makeBlue(){
     $('span.badge').removeClass('backGrey backGreen backFavorite backOrange backRed').addClass('backBlue');
-    $("th").eq(4).blueify().text('Available');
+    $("th").eq(4).blueify();
     $("th").eq(5).neutralize();
     $("#ToggleArrow").blueify();
 }
 function makePurple(){
     $('span.badge').removeClass('backGrey backGreen  backRed backOrange backBlue').addClass('backFavorite');
-    $("th").eq(4).tealify().text("Favorite");
-    $("th").eq(5).darkGrey().text('wdyt?');
+    $("th").eq(4).tealify();
+    $("th").eq(5).tealify();
     $('td.glyphicon.glyphicon-bookmark').tealify();
     $("#ToggleArrow").tealify();
 }
 function makeAllGrey() {
     $('span.badge').removeClass('backGreen backFavorite backBlue backOrange backRed').addClass('backGrey');
-    $("th").eq(4).lightGrey().text('All');
-    $("th").eq(5).darkGrey().text('wdyt?');
+    $("th").eq(4).lightGrey();
+    $("th").eq(5).lightGrey();
     $("#ToggleArrow").lightGrey();
 }
 $( "#ToggleArrow" ).click(
